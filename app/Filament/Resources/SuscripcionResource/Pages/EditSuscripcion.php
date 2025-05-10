da59c49fdc44174f4f79aa409d4dd59912f27d1a
Giusti89 <giustiever17@gmail.com>
2025-05-10 00:44:28 -0400

    modificacion editsuscription

--- app/Filament/Resources/SuscripcionResource/Pages/EditSuscripcion.php
+++ app/Filament/Resources/SuscripcionResource/Pages/EditSuscripcion.php
@@ -16,6 +16,24 @@ class EditSuscripcion extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+    protected function afterSave(): void
+    {
+        $suscripcion = $this->record;
+
+        
+        $spot = \App\Models\Spot::where('suscripcion_id', $suscripcion->id)->first();
+
+        // Verificamos que exista un spot y que el paquete tenga una landing asociada
+        if ($spot && $suscripcion->paquete && $suscripcion->paquete->landing) {
+            $nuevoTipoLanding = $suscripcion->paquete->landing->id;
+
+            // Actualizamos solo si cambió
+            if ($spot->tipolanding != $nuevoTipoLanding) {
+                $spot->tipolanding = $nuevoTipoLanding;
+                $spot->save();
+            }
+        }
+    }
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
