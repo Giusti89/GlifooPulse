cac6ca714f71533a621d3889f96347bc159596ae
Giusti89 <giustiever17@gmail.com>
2026-07-30 16:11:39 -0400

    cambio en texto enriquecido boton compartir

--- resources/views/share/producto.blade.php
+++ resources/views/share/producto.blade.php
@@ -3,22 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $meta['titulo'] }}</title>
-    <meta name="description" content="{{ $meta['descripcion'] }}">
-    <meta property="og:title" content="{{ $meta['titulo'] }}">
-    <meta property="og:description" content="{{ $meta['descripcion'] }}">
-    <meta property="og:image" content="{{ $meta['imagen'] }}">
-    <meta property="og:image:secure_url" content="{{ $meta['imagen'] }}">
-    <meta property="og:url" content="{{ request()->url() }}">
-    <meta property="og:type" content="product">
-    <meta property="og:site_name" content="glifoo.org">
+    <meta property="og:title" content="{{ $meta['titulo'] }}" />
+    <meta property="og:description" content="{{ $meta['descripcion'] }}" />
+    <meta property="og:image" content="{{ $meta['imagen'] }}" />
+    <meta property="og:url" content="{{ url()->current() }}" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="{{ $meta['titulo'] }}">
+    <meta name="twitter:description" content="{{ $meta['descripcion'] }}">
+    <meta name="twitter:image" content="{{ $meta['imagen'] }}">
 
-    
+    <title>{{ $meta['titulo'] }}</title>
     <script>
         window.location.href = "{{ $meta['url_destino'] }}";
     </script>
 </head>
 <body>
-    <p>Cargando detalles del producto...</p>
+    <p>Redirigiendo al producto... Si no eres redirigido, <a href="{{ $meta['url_destino'] }}">haz clic aquí</a>.</p>
 </body>
 </html>
