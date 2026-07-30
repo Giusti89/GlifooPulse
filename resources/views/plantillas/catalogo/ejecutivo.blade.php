ff0a5e1c7d98e21ab1cc6a0fdd127a3716e1dbe8
Giusti89 <giustiever17@gmail.com>
2026-07-30 16:32:17 -0400

    cambio asset en imagen del producto

--- resources/views/plantillas/catalogo/ejecutivo.blade.php
+++ resources/views/plantillas/catalogo/ejecutivo.blade.php
@@ -144,10 +144,10 @@
                                                      </button>
                                                  @endif
                                                  <button type="button" class="producto-compartir"
-                                                     data-url="{{ request()->url() }}?prod={{ $producto->slug }}#prod-{{ $producto->slug }}"
+                                                     data-url="{{ route('producto.compartir.enlace', ['spot_slug' => $spot->slug, 'product_slug' => $producto->slug]) }}"
                                                      data-titulo="{{ $producto->nombre }}"
                                                      data-descripcion="{{ Str::limit($producto->descripcion, 100) }}"
-                                                     data-imagen="{{ asset($src) }}"
+                                                     data-imagen="{{ asset($src) }}" 
                                                      onclick="compartirProducto(this)">
                                                      Compartir
                                                  </button>
